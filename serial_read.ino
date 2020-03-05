f59c4c0bfdb6b0a7af86fe0f66d3c842a9c14cbd
Nícollas de Oliveira <50387444+oacin@users.noreply.github.com>
2020-03-05 17:00:45 -0300

    Add files via upload

---
+++ serial_read.ino
@@ -0,0 +1,44 @@
+//serial communication library for external devices that isnt an arduino board
+#include <SoftwareSerial.h>
+
+//variable to store each char in the scanner buffer
+char c;
+//variable to concatenate all char readings in one string of text
+String text = "";
+
+//uses pin 10 and 11 for communication between the scanner and arduino
+SoftwareSerial sc(10 , 11); // TX, RX
+
+void setup() 
+{
+  //starts arduino serial
+  Serial.begin(9600);
+  //starts scanner serial
+  sc.begin(9600);
+  Serial.println("Connected!");
+}
+
+void loop()
+{
+  //while scanner buffer is available
+  while (sc.available() != NULL) 
+  {
+    //reads the char in the buffer[n] index
+    c = sc.read();
+
+    //if read char isnt a carriage return 
+    if (c != 13)
+    {
+      //concatenate the char read into the text string 
+      text.concat(c);
+    }
+    //if read char is a carriage return, indicates that it's the end the buffer
+    else
+    {
+      //prints the stored text
+      Serial.println(text);
+      //clear the text value
+      text = "";
+    }
+  }
+}
